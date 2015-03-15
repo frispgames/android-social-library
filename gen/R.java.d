8762a8cf54d52a4a74bba8ce67f21a5bfa726d71
Michael Treacher <m.w.treacher@gmail.com>
2015-03-15 18:58:54 +1100

    Re made the jar

--- gen/R.java.d
+++ gen/R.java.d
@@ -4,7 +4,4 @@
 /Users/treach3r/Unity/android-plugins/frisp-social/res/drawable-mdpi/ic_launcher.png \
 /Users/treach3r/Unity/android-plugins/frisp-social/res/layout/main.xml \
 /Users/treach3r/Unity/android-plugins/frisp-social/res/values/strings.xml \
-/Users/treach3r/Unity/android-plugins/frisp-social/bin/res/drawable-hdpi/ic_launcher.png \
-/Users/treach3r/Unity/android-plugins/frisp-social/bin/res/drawable-ldpi/ic_launcher.png \
-/Users/treach3r/Unity/android-plugins/frisp-social/bin/res/drawable-mdpi/ic_launcher.png \
 /Users/treach3r/Unity/android-plugins/frisp-social/bin/AndroidManifest.xml \
